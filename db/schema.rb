c37d3b5daf8b2600793facba35e5a5d682f9c780
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2024-02-22 23:44:32 -0300

    Fix linter errors

--- db/schema.rb
+++ db/schema.rb
@@ -1,17 +1,7 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
+require 'active_record'
 
+# rubocop:disable Metrics/BlockLength
 ActiveRecord::Schema[7.1].define(version: 20_240_222_003_114) do
-  # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
   create_table 'admin_groups', force: :cascade do |t|
@@ -62,3 +52,4 @@ ActiveRecord::Schema[7.1].define(version: 20_240_222_003_114) do
   add_foreign_key 'visits', 'properties'
   add_foreign_key 'visits', 'users'
 end
+# rubocop:enable Metrics/BlockLength
