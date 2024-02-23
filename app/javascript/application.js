f90dddcf8205cba111bc4bf5341ab6644082c0db
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2024-02-22 23:00:18 -0300

    Creates the migrations for users, admin groups, properties, visits and add the qr code column to the properties table

--- app/javascript/application.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './components/App';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
-  createRoot(root).render(<App />);
-});
--- config/initializers/cors.rb
+++
@@ -1,8 +0,0 @@
-# config/initializers/cors.rb
-
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'localhost:8080' # Ajusta esto al puerto/dominio de tu aplicación React en desarrollo
-    resource '*', headers: :any, methods: %i[get post patch put delete options head]
-  end
-end
