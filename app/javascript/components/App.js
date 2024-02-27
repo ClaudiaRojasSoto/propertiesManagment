a1bd7934ee6335c1056e7afb23effac96e08e203
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2024-02-27 16:01:11 -0300

    add the provider to the code

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,20 +1,15 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
-import { greetUser } from '../redux/actions/greetingsActions';
 
 function App() {
-  useEffect(() => {
-    store.dispatch(greetUser('John'));
-  }, []);
-
   return (
     <Provider store={store}>
       <Router>
         <Routes>
-        <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
         </Routes>
       </Router>
     </Provider>
