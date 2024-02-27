8fe1d63f659d1d0f6708e9a4f6b02e7ce3828ce8
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2024-02-27 15:59:19 -0300

    Add the login action

--- app/javascript/redux/actions/authActions.js
+++ app/javascript/redux/actions/authActions.js
@@ -1,8 +1,8 @@
-// Actions type
+import axios from 'axios';
+
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 
-// Login Action
 export const login = (email, password) => async dispatch => {
   try {
     const response = await axios.post('http://localhost:3000/api/v1/login', { email, password });
