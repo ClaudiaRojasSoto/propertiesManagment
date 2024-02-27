eeb6ec3b7edbf7eaec5e195914e152186b4da4a7
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2024-02-27 16:02:07 -0300

    Add the provider

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import store from './redux/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  createRoot(root).render(<App />);
+  if (root !== null) {
+    createRoot(root).render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  }
 });
