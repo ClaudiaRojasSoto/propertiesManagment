c70daff0025a36aabeed0a739fcfe2f22ac5acb2
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2024-02-29 21:38:35 -0300

    Add the open lettener configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.mandrillapp.com',
+    port: 587,
+    user_name: ENV.fetch('MANDRILL_USERNAME', nil),
+    password: ENV.fetch('MANDRILL_API_KEY', nil),
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
+  # Configura default_url_options para reflejar tu dominio de producción
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
