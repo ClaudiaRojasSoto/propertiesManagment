061b2fa0ccef4a1f5e4e70af69016403a9996d39
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2024-02-29 21:39:17 -0300

    Update the routes to add a get route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'reset-password', to: 'password_resets#reset_password'
       post 'login', to: 'authentication#login'
       post 'signup', to: 'authentication#register'
       post 'password/forgot', to: 'password_resets#forgot_password'
